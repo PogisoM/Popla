ff735a52e95ae4ae6ad1a77e1271627df1ae0561
PogisoM <165954719+PogisoM@users.noreply.github.com>
2024-04-16 21:53:49 +0200

    updated js

--- index.js
+++ index.js
@@ -54,5 +54,3 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Paris");
